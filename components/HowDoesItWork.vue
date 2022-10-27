e233de2a1fb5a20fd92d411eb17fa6f0ab2c27cb
MilkaChokolate <milenapolyanichko@yandex.ru>
2022-10-27 18:11:05 +0300

    .

--- components/HowDoesItWork.vue
+++ components/HowDoesItWork.vue
@@ -2,7 +2,7 @@
 <div class="howItWork">
   <h1>How does it work?</h1>
 
-  <div class="howItWork-1 d-flex justify-content-center flex-wrap align-items-center">
+  <div class="step-1 d-flex justify-content-center flex-wrap align-items-center">
     <div class="col-lg-5 col-md-5 col-sm-5 d-flex align-items-center justify-content-center">
      <img src="../static/howWork1.png" class="col-lg-8 col-md-12 col-sm-12">
     </div>
@@ -18,8 +18,11 @@
     </div>
   </div>
 
-  <div class="howItWork-2 d-flex justify-content-center flex-wrap align-items-center">
-    <div class="col-lg-5 col-md-5 col-sm-5">
+  <div class="step-2 d-flex justify-content-center flex-wrap align-items-center">
+    <div class="col-lg-5 col-md-5 col-sm-5 d-flex align-items-center justify-content-center">
+      <img src="../static/howWork2.png" class="col-lg-8 col-md-12 col-sm-12">
+    </div>
+    <div class="col-lg-5 col-md-5 col-sm-5 order-first">
       <h2>Metamask x Tornado Cash</h2>
       <p>Your funds go into the smart contract as yours but they exit named Blank.
         We use cryptographic proofs to guarantee that no one can link your withdrawals to deposits,
@@ -32,12 +35,9 @@
         <li>Partial Withdraw</li>
       </ol>
     </div>
-    <div class="col-lg-5 col-md-5 col-sm-5 d-flex align-items-center justify-content-center">
-      <img src="../static/howWork2.png" class="col-lg-8 col-md-12 col-sm-12">
-    </div>
   </div>
 
-  <div class="howItWork-3 d-flex justify-content-center align-items-center flex-wrap">
+  <div class="step-3 d-flex justify-content-center align-items-center flex-wrap">
     <div class="col-lg-5 col-md-5 col-sm-5 d-flex align-items-center justify-content-center">
      <img src="../static/howWork3.png" class="col-lg-8 col-md-12 col-sm-12">
     </div>
@@ -46,6 +46,25 @@
       <p>Create a wallet, receive a backup seed key,
         deposit your crypto $$$ and let automation do the rest - we'll mix your funds with the pool
         of coins in Blank to hide their origin.
+      </p>
+      <ol>
+        <li>ETH, WBTC and all ERC-20 support</li>
+        <li>ETH, WBTC and all ERC-20 support</li>
+        <li>Partial Withdraw</li>
+      </ol>
+    </div>
+  </div>
+
+  <div class="step-4 d-flex justify-content-center flex-wrap align-items-center">
+    <div class="col-lg-5 col-md-5 col-sm-5 d-flex align-items-center justify-content-center">
+      <img src="../static/howWork2.png" class="col-lg-8 col-md-12 col-sm-12">
+    </div>
+    <div class="col-lg-5 col-md-5 col-sm-5 order-first">
+      <h2>Metamask x Tornado Cash</h2>
+      <p>Your funds go into the smart contract as yours but they exit named Blank.
+        We use cryptographic proofs to guarantee that no one can link your withdrawals to deposits,
+        unless you want them to. In technical language, we use the zkSnark technology to do it.
+        Only the user gets the secret to a given deposit, which allows to withdraw the correlating funds from Blank under a new address.
       </p>
       <ol>
         <li>ETH, WBTC and all ERC-20 support</li>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,30 +1,30 @@
 <template>
-  <div>
-    <nav class="navbar navbar-expand-lg" style="background: #E7F1FB; position: fixed; width: 100%; z-index: 100">
-      <div class="container-fluid">
-          <a href="#" class="navbar-brand">
-              <img src="../static/Frame.png" style="width: 102px; height: 27px">
-          </a>
-          <button type="button" class="navbar-toggler" data-bs-toggle="collapse" data-bs-target="#navbarCollapse">
-              <span class="navbar-toggler-icon">
-                <img src="../static/list.svg" style="width: 27px; height: 27px">
-              </span>
-          </button>
-          <div class="collapse navbar-collapse justify-content-end" id="navbarCollapse">
-              <div class="navbar-nav">
-                  <a href="#" class="nav-item nav-link">Why Blank</a>
-                  <a href="#" class="nav-item nav-link">What blank offers</a>
-                  <a href="#" class="nav-item nav-link">How it works</a>
-                  <a href="#" class="nav-item nav-link">Blank token</a>
-                  <button href="#" class="btn btn-secondary">Token Metrics</button>
-              </div>
-          </div>
+<div>
+  <nav class="navbar navbar-expand-lg" style="background: #E7F1FB; position: fixed; width: 100%; z-index: 100">
+    <div class="container-fluid">
+      <a href="#" class="navbar-brand">
+        <img src="../static/Frame.png" style="width: 102px; height: 27px">
+      </a>
+      <button type="button" class="navbar-toggler" data-bs-toggle="collapse" data-bs-target="#navbarCollapse">
+        <span class="navbar-toggler-icon">
+          <svg xmlns="http://www.w3.org/2000/svg" width="27" height="27" fill="currentColor" class="bi bi-list" viewBox="0 0 16 16">
+            <path fill-rule="evenodd" d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5z"/>
+          </svg>
+        </span>
+      </button>
+      <div class="collapse navbar-collapse justify-content-end" id="navbarCollapse">
+        <div class="navbar-nav">
+          <a href="#" class="nav-item nav-link">Why Blank</a>
+          <a href="#" class="nav-item nav-link">How it works</a>
+          <a href="#" class="nav-item nav-link">Pricing</a>
+          <a href="#" class="nav-item nav-link">Feedback</a>
+        </div>
       </div>
-    </nav>
+    </div>
+  </nav>
 
-    <Nuxt></Nuxt>
-  </div>
-
+  <Nuxt></Nuxt>
+</div>
 </template>
 
 <script>
